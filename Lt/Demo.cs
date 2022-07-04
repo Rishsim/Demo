266a28e6595e678544b7ae8c3498a3332c78da81
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-07-04 23:38:10 +0100

    testing code added

---
+++ Lt/Demo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lt
+{
+    public class Demo
+    {
+        public void A1()
+        {
+            List<int> b = new List<int>();
+
+            b.Add(1);
+
+            b.Add(6);
+
+            b.Add(22);
+            b.Add(90);
+
+            foreach (int a in b) ;
+
+            Console.WriteLine(b);
+        
+        
+        
+        }
+
+
+
+
+
+     }
+
+
+}
---
+++ Lt/Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lt
+{
+    internal class Sample
+    {
+        static void Main(string[] args)
+        {
+            Demo q = new Demo();
+
+            q.A1();
+
+
+        }
+
+
+
+    }
+}
